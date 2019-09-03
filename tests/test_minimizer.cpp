dc2d7c5480abd259e961d68590c95253a7fb9f98
Jouni Siren <jouni.siren@iki.fi>
2019-09-03 11:16:04 +0900

    Error messages for MinimizerIndex tests

--- tests/test_minimizer.cpp
+++ tests/test_minimizer.cpp
@@ -15,7 +15,6 @@ namespace
 
 //------------------------------------------------------------------------------
 
-
 MinimizerIndex::minimizer_type
 get_minimizer(MinimizerIndex::key_type key, MinimizerIndex::offset_type offset = 0, bool orientation = false)
 {
@@ -30,9 +29,9 @@ TEST(ObjectManipulation, EmptyIndex)
   MinimizerIndex default_copy(default_index);
   MinimizerIndex alt_index(15, 6);
   MinimizerIndex alt_copy(alt_index);
-  EXPECT_EQ(default_index, default_copy);
-  EXPECT_EQ(alt_index, alt_copy);
-  EXPECT_NE(default_index, alt_index);
+  EXPECT_EQ(default_index, default_copy) << "A copy of the default index is not identical to the original";
+  EXPECT_EQ(alt_index, alt_copy) << "A copy of a parametrized index is not identical to the original";
+  EXPECT_NE(default_index, alt_index) << "Default and parametrized indexes are identical";
 }
 
 TEST(ObjectManipulation, Contents)
@@ -42,15 +41,15 @@ TEST(ObjectManipulation, Contents)
 
   // Different contents.
   default_index.insert(get_minimizer(1), make_pos_t(1, false, 3));
-  EXPECT_NE(default_index, default_copy);
+  EXPECT_NE(default_index, default_copy) << "Empty index is identical to nonempty index";
 
   // Same key, different value.
   default_copy.insert(get_minimizer(1), make_pos_t(2, false, 3));
-  EXPECT_NE(default_index, default_copy);
+  EXPECT_NE(default_index, default_copy) << "Indexes with different values are identical";
 
   // Same contents.
   default_copy = default_index;
-  EXPECT_EQ(default_index, default_copy);
+  EXPECT_EQ(default_index, default_copy) << "A copy of a nonempty index is not identical to the original";
 }
 
 TEST(ObjectManipulation, Swap)
@@ -61,10 +60,10 @@ TEST(ObjectManipulation, Swap)
 
   MinimizerIndex first_copy(first), second_copy(second);
   first.swap(second);
-  EXPECT_NE(first, first_copy);
-  EXPECT_EQ(first, second_copy);
-  EXPECT_EQ(second, first_copy);
-  EXPECT_NE(second, second_copy);
+  EXPECT_NE(first, first_copy) << "Swapping did not change the first index";
+  EXPECT_EQ(first, second_copy) << "The first index was not swapped correctly";
+  EXPECT_EQ(second, first_copy) << "The second index was not swapped correctly";
+  EXPECT_NE(second, second_copy) << "Swapping did not change the second index";
 }
 
 TEST(ObjectManipulation, Serialization)
@@ -85,7 +84,7 @@ TEST(ObjectManipulation, Serialization)
   in.close();
   gbwt::TempFile::remove(filename);
 
-  EXPECT_EQ(index, copy);
+  EXPECT_EQ(index, copy) << "Loaded index is not identical to the original";
 }
 
 //------------------------------------------------------------------------------
@@ -116,7 +115,7 @@ TEST_F(MinimizerExtraction, LeftmostOccurrence)
   MinimizerIndex index(3, 2);
   MinimizerIndex::minimizer_type correct = get_minimizer(0 * 16 + 0 * 4 + 3 * 1, 2); // AAT
   MinimizerIndex::minimizer_type result = index.minimizer(this->str.begin(), this->str.end());
-  EXPECT_EQ(result, correct);
+  EXPECT_EQ(result, correct) << "The leftmost minimizer was not found";
 }
 
 TEST_F(MinimizerExtraction, AllMinimizers)
@@ -133,7 +132,7 @@ TEST_F(MinimizerExtraction, AllMinimizers)
     get_minimizer(0 * 16 + 1 * 4 + 3 * 1, 10, false)  // ACT
   };
   std::vector<MinimizerIndex::minimizer_type> result = index.minimizers(this->str.begin(), this->str.end());
-  EXPECT_EQ(result, correct);
+  EXPECT_EQ(result, correct) << "Did not find the correct minimizers";
 }
 
 TEST_F(MinimizerExtraction, WindowLength)
@@ -147,7 +146,7 @@ TEST_F(MinimizerExtraction, WindowLength)
     get_minimizer(3 * 16 + 0 * 4 + 3 * 1, 10, true)   // TAT
   };
   std::vector<MinimizerIndex::minimizer_type> result = index.minimizers(this->str.begin(), this->str.end());
-  EXPECT_EQ(result, correct);
+  EXPECT_EQ(result, correct) << "Did not find the correct minimizers";
 }
 
 TEST_F(MinimizerExtraction, InvalidCharacters)
@@ -164,7 +163,7 @@ TEST_F(MinimizerExtraction, InvalidCharacters)
     get_minimizer(0 * 16 + 1 * 4 + 3 * 1, 10, false)  // ACT
   };
   std::vector<MinimizerIndex::minimizer_type> result = index.minimizers(weird.begin(), weird.end());
-  EXPECT_EQ(result, correct);
+  EXPECT_EQ(result, correct) << "Did not find the correct minimizers";
 }
 
 TEST_F(MinimizerExtraction, BothOrientations)
@@ -172,14 +171,14 @@ TEST_F(MinimizerExtraction, BothOrientations)
   MinimizerIndex index(3, 2);
   std::vector<MinimizerIndex::minimizer_type> forward_minimizers = index.minimizers(this->str.begin(), this->str.end());
   std::vector<MinimizerIndex::minimizer_type> reverse_minimizers = index.minimizers(this->rev.begin(), this->rev.end());
-  ASSERT_EQ(forward_minimizers.size(), reverse_minimizers.size());
+  ASSERT_EQ(forward_minimizers.size(), reverse_minimizers.size()) << "Different number of minimizers in forward and reverse orientations";
   for(size_t i = 0; i < forward_minimizers.size(); i++)
   {
     MinimizerIndex::minimizer_type& f = forward_minimizers[i];
     MinimizerIndex::minimizer_type& r = reverse_minimizers[forward_minimizers.size() - 1 - i];
-    EXPECT_EQ(f.key, r.key);
-    EXPECT_EQ(f.offset, this->str.length() - 1 - r.offset);
-    EXPECT_NE(f.is_reverse, r.is_reverse);
+    EXPECT_EQ(f.key, r.key) << "Wrong key for minimizer " << i;
+    EXPECT_EQ(f.offset, this->str.length() - 1 - r.offset) << "Wrong offset for minimizer " << i;
+    EXPECT_NE(f.is_reverse, r.is_reverse) << "Wrong orientation for minimizer " << i;
   }
 }
 
@@ -202,15 +201,15 @@ public:
   void check_minimizer_index(const MinimizerIndex& index, const std::map<size_t, std::set<pos_t>>& correct_values,
                              size_t keys, size_t values, size_t unique)
   {
-    ASSERT_EQ(index.size(), keys);
-    ASSERT_EQ(index.values(), values);
-    EXPECT_EQ(index.unique_keys(), unique);
+    ASSERT_EQ(index.size(), keys) << "Wrong number of keys";
+    ASSERT_EQ(index.values(), values) << "Wrong number of values";
+    EXPECT_EQ(index.unique_keys(), unique) << "Wrong number of unique keys";
 
     for(auto iter = correct_values.begin(); iter != correct_values.end(); ++iter)
     {
       std::vector<pos_t> result = index.find(get_minimizer(iter->first));
       std::vector<pos_t> correct(iter->second.begin(), iter->second.end());
-      EXPECT_EQ(result, correct);
+      EXPECT_EQ(result, correct) << "Wrong positions for key " << iter->first;
     }
   }
 };
@@ -240,7 +239,7 @@ TEST_F(CorrectKmers, MissingKeys)
   }
   for(size_t i = this->total_keys + 1; i <= 2 * this->total_keys; i++)
   {
-    EXPECT_TRUE(index.find(get_minimizer(i)).empty());
+    EXPECT_TRUE(index.find(get_minimizer(i)).empty()) << "Nonempty value for key " << i;
   }
 }
 
@@ -249,10 +248,10 @@ TEST_F(CorrectKmers, EmptyKeysValues)
   MinimizerIndex index;
 
   index.insert(get_minimizer(MinimizerIndex::NO_KEY), make_pos_t(1, false, 0));
-  EXPECT_TRUE(index.find(get_minimizer(MinimizerIndex::NO_KEY)).empty());
+  EXPECT_TRUE(index.find(get_minimizer(MinimizerIndex::NO_KEY)).empty()) << "Nonempty value for empty key";
 
   index.insert(get_minimizer(this->total_keys + 1), MinimizerIndex::decode(MinimizerIndex::NO_VALUE));
-  EXPECT_TRUE(index.find(get_minimizer(this->total_keys + 1)).empty());
+  EXPECT_TRUE(index.find(get_minimizer(this->total_keys + 1)).empty()) << "Nonempty value after inserting empty value";
 }
 
 TEST_F(CorrectKmers, MultipleOccurrences)
@@ -327,7 +326,7 @@ TEST_F(CorrectKmers, Rehashing)
     correct_values[i].insert(pos);
     keys++; values++; unique++;
   }
-  ASSERT_EQ(index.max_keys(), threshold);
+  ASSERT_EQ(index.max_keys(), threshold) << "Index capacity changed at threshold";
 
   {
     size_t i = threshold + 1;
@@ -336,7 +335,7 @@ TEST_F(CorrectKmers, Rehashing)
     correct_values[i].insert(pos);
     keys++; values++; unique++;
   }
-  EXPECT_GT(index.max_keys(), threshold);
+  EXPECT_GT(index.max_keys(), threshold) << "Index capacity not increased after threshold";
 
   this->check_minimizer_index(index, correct_values, keys, values, unique);
 }
