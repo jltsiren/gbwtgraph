e4d9814b4f6f4fc7e9c653195a453f49ce29dd64
Jouni Siren <jouni.siren@iki.fi>
2020-03-17 00:12:57 -0700

    Some groundwork for a better path cover algorithm

--- include/gbwtgraph/path_cover.h
+++ include/gbwtgraph/path_cover.h
@@ -72,6 +72,22 @@ gbwt::GBWT local_haplotypes(const GBWTGraph& graph,
 
 //------------------------------------------------------------------------------
 
+/*
+  Return the weakly connected components in the graph. The components are sorted by
+  the minimum node id, and the node ids in each component are also sorted.
+*/
+std::vector<std::vector<nid_t>> weakly_connected_components(const HandleGraph& graph);
+
+/*
+  Determine whether the given component is acyclic in a nice way. In such graphs,
+  when we start from nodes with indegree 0 in forward orientation, we reach each node
+  in a single orientation and find no cycles. Return the head nodes when the component
+  passes the tests or an empty vector otherwise.
+*/
+std::vector<nid_t> is_nice_and_acyclic(const HandleGraph& graph, const std::vector<nid_t>& component);
+
+//------------------------------------------------------------------------------
+
 } // namespace gbwtgraph
 
 #endif // GBWTGRAPH_PATH_COVER_H
--- tests/test_path_cover.cpp
+++ tests/test_path_cover.cpp
@@ -16,6 +16,76 @@ namespace
 
 //------------------------------------------------------------------------------
 
+class GraphAlgorithmTest : public ::testing::Test
+{
+public:
+  gbwt::GBWT index;
+  GBWTGraph graph;
+  size_t components;
+  std::vector<std::set<gbwt::vector_type>> correct_paths;
+
+  GraphAlgorithmTest()
+  {
+  }
+
+  void SetUp() override
+  {
+    auto gfa_parse = gfa_to_gbwt("components.gfa");
+    this->index = *(gfa_parse.first);
+    this->graph = GBWTGraph(this->index, *(gfa_parse.second));
+    this->components = 2;
+  }
+};
+
+TEST_F(GraphAlgorithmTest, Components)
+{
+  std::vector<std::vector<nid_t>> correct_components =
+  {
+    { 11, 12, 13, 14, 15, 16, 17 },
+    { 21, 22, 23, 24, 25 }
+  };
+  std::vector<std::vector<nid_t>> result = weakly_connected_components(this->graph);
+  ASSERT_EQ(result.size(), correct_components.size()) << "Wrong number of components";
+
+  for(size_t i = 0; i < result.size(); i++)
+  {
+    ASSERT_EQ(result[i].size(), correct_components[i].size()) << "Wrong number of nodes in component " << i;
+    auto result_iter = result[i].begin();
+    auto correct_iter = correct_components[i].begin();
+    while(result_iter != result[i].end())
+    {
+      EXPECT_EQ(*result_iter, *correct_iter) << "Incorrect node in component " << i;
+      ++result_iter; ++correct_iter;
+    }
+  }
+}
+
+TEST_F(GraphAlgorithmTest, HeadNodes)
+{
+  std::vector<std::vector<nid_t>> correct_heads =
+  {
+    { 11 },
+    { }
+  };
+  std::vector<std::vector<nid_t>> components = weakly_connected_components(this->graph);
+  ASSERT_EQ(components.size(), correct_heads.size()) << "Wrong number of components";
+
+  for(size_t i = 0; i < components.size(); i++)
+  {
+    std::vector<nid_t> heads = is_nice_and_acyclic(this->graph, components[i]);
+    ASSERT_EQ(heads.size(), correct_heads[i].size()) << "Wrong number of head nodes in component " << i;
+    auto result_iter = heads.begin();
+    auto correct_iter = correct_heads[i].begin();
+    while(result_iter != heads.end())
+    {
+      EXPECT_EQ(*result_iter, *correct_iter) << "Incorrect head node in component " << i;
+      ++result_iter; ++correct_iter;
+    }
+  }
+}
+
+//------------------------------------------------------------------------------
+
 class PathCoverTest : public ::testing::Test
 {
 public:
