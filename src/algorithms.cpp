04db538f40431d8497c22e52cc188383ce8fa488
Jouni Siren <jouni.siren@iki.fi>
2024-09-23 11:26:10 -0700

    Initial path LCS algorithm

--- src/algorithms.cpp
+++ src/algorithms.cpp
@@ -1,6 +1,7 @@
 #include <gbwtgraph/algorithms.h>
 
 #include <limits>
+#include <map>
 #include <stack>
 #include <unordered_map>
 
@@ -235,6 +236,224 @@ topological_order(const HandleGraph& graph, const std::unordered_set<nid_t>& sub
   }
 
   if(result.size() != 2 * (subgraph.size() - missing_nodes)) { result.clear(); }
+  return result;
+}
+
+//------------------------------------------------------------------------------
+
+/*
+  A point in the dynamic programming matrix for LCS computation.
+
+  Each point represents the furthest point reached with the given number of weighted
+  edits on a weighted diagonal. The point itself points to the first unmatched pair
+  of nodes.
+
+  TODO: We could save space by using 32-bit integers.
+  TODO: We could derive the weight using the invariant.
+  TODO: One of the offsets is redunant.
+*/
+struct LCSPoint
+{
+  // Twice the total length of the matched nodes.
+  // The invariant is: weight + edits = a_prefix_sum + b_prefix_sum.
+  size_t weight;
+
+  // Non-weighted offsets in the two sequences.
+  size_t a_offset, b_offset;
+
+  // Length of the non-weighted run of matches ending at this point.
+  size_t matches;
+};
+
+/*
+  Dynamic programming matrix for LCS computation.
+
+  Because the LCS is weighted by sequence length, we precompute prefix sums of weights
+  over both paths. The algorithm is a variant of Myers' O(nd) algorithm. We therefore
+  store only the furthest points reached with each (weighted) number of edits on each
+  (weighted) diagonal.
+
+  TODO: We could save space by using 32-bit integers.
+*/
+struct LCSMatrix
+{
+  const gbwt::vector_type& a;
+  const gbwt::vector_type& b;
+
+  // Prefix sums of sequence lengths on the two paths.
+  std::vector<size_t> a_prefix_sum, b_prefix_sum;
+
+  // The final result if we have found it.
+  LCSPoint result;
+
+  // Furthest point reached for each (weighted number of edits, weighted diagonal).
+  // A diagonal is a_prefix_sum - b_prefix_sum.
+  // The point refers to the first unmatched pair of nodes.
+  std::map<std::pair<size_t, std::int64_t>, LCSPoint> points;
+
+  LCSMatrix(const GBWTGraph& graph, const gbwt::vector_type& a, const gbwt::vector_type& b) :
+    a(a), b(b), a_prefix_sum(a.size() + 1, 0), b_prefix_sum(b.size() + 1, 0),
+    result({ 0, 0, 0, 0 })
+  {
+    for(size_t i = 1; i <= a.size(); i++)
+    {
+      this->a_prefix_sum[i] = this->a_prefix_sum[i - 1] + graph.get_length(GBWTGraph::node_to_handle(a[i - 1]));
+    }
+    for(size_t i = 1; i <= b.size(); i++)
+    {
+      this->b_prefix_sum[i] = this->b_prefix_sum[i - 1] + graph.get_length(GBWTGraph::node_to_handle(b[i - 1]));
+    }
+  }
+
+  // Returns the sorted set of diagonals for the given number of edits.
+  std::vector<std::int64_t> diagonals(size_t edits) const
+  {
+    auto low = this->points.lower_bound(std::make_pair(edits, std::numeric_limits<std::int64_t>::min()));
+    auto high = this->points.lower_bound(std::make_pair(edits + 1, std::numeric_limits<std::int64_t>::min()));
+    std::vector<std::int64_t> result;
+    for(auto iter = low; iter != high; ++iter)
+    {
+      result.push_back(iter->first.second);
+    }
+  }
+
+  // Inserts the point if it is better than the existing one.
+  void try_insert(size_t edits, std::int64_t diagonal, LCSPoint point)
+  {
+    auto iter = this->points.find(std::make_pair(edits, diagonal));
+    if(iter == this->points.end())
+    {
+      this->points[std::make_pair(edits, diagonal)] = point;
+    }
+    else if(point.weight > iter->second.weight)
+    {
+      iter->second = point;
+    }
+  }
+
+  size_t a_weight(size_t offset) const
+  {
+    return this->a_prefix_sum[offset + 1] - this->a_prefix_sum[offset];
+  }
+
+  size_t b_weight(size_t offset) const
+  {
+    return this->b_prefix_sum[offset + 1] - this->b_prefix_sum[offset];
+  }
+
+  // Extends matches over all diagonals for the given number of weighted edits.
+  // Also adds successor points reachable with a single edit from each extension.
+  // Returns true and sets the point if we reached the end.
+  bool extend(size_t edits)
+  {
+    std::vector<std::int64_t> diagonals = this->diagonals(edits);
+    for(std::int64_t diagonal : diagonals)
+    {
+      auto iter = this->points.find(std::make_pair(edits, diagonal));
+      if(iter == this->points.end()) { continue; }
+      LCSPoint point = iter->second; // Copy the point to avoid iterator invalidation.
+      while(point.a_offset < this->a.size() && point.b_offset < this->b.size() && this->a[point.a_offset] == this->b[point.b_offset])
+      {
+        point.weight += 2 * this->a_weight(point.a_offset);
+        point.a_offset++; point.b_offset++; point.matches++;
+      }
+      if(point.matches > 0) { this->points[std::make_pair(edits, diagonal)] = point; }
+      if(point.a_offset == this->a.size() || point.b_offset == this->b.size())
+      {
+        this->result = point;
+        return true;
+      }
+      if(point.a_offset < this->a.size())
+      {
+        size_t weight = this->a_weight(point.a_offset);
+        LCSPoint new_point { point.weight, point.a_offset + 1, point.b_offset, 0 };
+        this->try_insert(edits + weight, diagonal + std::int64_t(weight), new_point);
+      }
+      if(point.b_offset < this->b.size())
+      {
+        size_t weight = this->b_weight(point.b_offset);
+        LCSPoint new_point { point.weight, point.a_offset, point.b_offset + 1, 0 };
+        this->try_insert(edits + weight, diagonal - std::int64_t(weight), new_point);
+      }
+    }
+    return false;
+  }
+
+  // Returns the next possible number of edits after the given number of edits.
+  // Assumes that extend() has already been called for the given number of edits.
+  // Returns std::numeric_limits<size_t>::max() if there are no more possible edits.
+  size_t next_edits(size_t edits) const
+  {
+    auto iter = this->points.lower_bound(std::make_pair(edits + 1, std::numeric_limits<std::int64_t>::min()));
+    return (iter == this->points.end() ? std::numeric_limits<size_t>::max() : iter->first.first);
+  }
+
+  // Returns the predecessor point and number of edits for the given point and number of edits.
+  std::pair<LCSPoint, size_t> predecessor(size_t a_offset, size_t b_offset, size_t edits) const
+  {
+    std::int64_t diagonal = std::int64_t(this->a_prefix_sum[a_offset]) - std::int64_t(this->b_prefix_sum[b_offset]);
+    auto prev = this->points.end();
+    if(a_offset > 0 && this->a_weight(a_offset - 1) > 0)
+    {
+      size_t weight = this->a_weight(a_offset - 1);
+      prev = this->points.find(std::make_pair(edits - weight, diagonal - std::int64_t(weight)));
+    }
+    auto next = this->points.end();
+    if(b_offset > 0 && this->b_weight(b_offset - 1) > 0)
+    {
+      size_t weight = this->b_weight(b_offset - 1);
+      next = this->points.find(std::make_pair(edits - weight, diagonal + std::int64_t(weight)));
+    }
+    if(prev != this->points.end() && next != this->points.end())
+    {
+      return (prev->second.weight > next->second.weight ?
+        std::make_pair(prev->second, prev->first.first) :
+        std::make_pair(next->second, next->first.first));
+    }
+    else if(prev != this->points.end())
+    {
+      return std::make_pair(prev->second, prev->first.first);
+    }
+    else if(next != this->points.end())
+    {
+      return std::make_pair(next->second, next->first.first);
+    }
+    else
+    {
+      return std::make_pair(LCSPoint { 0, 0, 0, 0 }, 0);
+    }
+  }
+};
+
+std::vector<std::pair<size_t, size_t>>
+path_lcs(const GBWTGraph& graph, const gbwt::vector_type& a, const gbwt::vector_type& b)
+{
+  if(a.empty() || b.empty()) { return {}; }
+
+  // Find the furthest point on each diagonal with each possible number of edits, until we reach the end.
+  LCSMatrix matrix(graph, a, b);
+  size_t edits = 0;
+  while(true)
+  {
+    if(matrix.extend(edits)) { break; }
+    edits = matrix.next_edits(edits);
+    if(edits == std::numeric_limits<size_t>::max()) { break; }
+  }
+
+  // Trace back the LCS.
+  std::vector<std::pair<size_t, size_t>> result;
+  LCSPoint point = matrix.result;
+  while(point.a_offset > 0 || point.b_offset > 0)
+  {
+    for(size_t i = 0; i < point.matches; i++)
+    {
+      point.a_offset--; point.b_offset--;
+      result.push_back(std::make_pair(a[point.a_offset], b[point.b_offset]));
+    }
+    std::tie(point, edits) = matrix.predecessor(point.a_offset, point.b_offset, edits);
+  }
+  std::reverse(result.begin(), result.end());
+
   return result;
 }
 
