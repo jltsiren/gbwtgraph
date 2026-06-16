7d0e462d3f29e2ab1078d67df4630c463baabb77
Jouni Siren <jouni.siren@iki.fi>
2026-06-16 16:34:11 -0700

    Make haplotype search more useful

--- src/gbz_find.cpp
+++ src/gbz_find.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <thread>
+#include <unordered_map>
 #include <vector>
 
 #include <getopt.h>
@@ -17,18 +18,18 @@ using namespace gbwtgraph;
   This tool finds all exact occurrences of the pattern in the haplotypes.
   It iterates over haplotype windows using `for_each_haplotype_window`, as in
   minimizer index construction. If a window contains the pattern, the tool
-  outputs three tab-separated fields:
+  outputs tab-separated fields:
 
   1. The path using GFA walk notation.
   2. The sequence as comma-separated node labels.
   3. Number of haplotypes containing the path.
+  4. Length of the missing suffix of the last node.
+  5. Reference coordinates for each node, if requested.
 
   Given a pattern of length k, the path consists of the initial oriented node
   followed by all k-1 bp extensions. If the pattern is found in the initial
   node, it is listed separately for each extension. For the final node, only
   the prefix required to complete the k-1 bp extension is included in the path.
-
-  TODO: Reference coordinates if the selected reference is one of the paths?
 */
 
 const std::string tool_name = "GBZ Haplotype Search";
@@ -39,12 +40,15 @@ struct Config
 
   std::string graph_name;
   std::string pattern;
+  std::string reference_sample;
 
   bool progress;
   size_t threads;
 
   constexpr static size_t BUFFER_SIZE = 1024; // Lines.
 };
+
+std::unordered_map<gbwt::node_type, std::string> reference_labels(const GBZ& graph, const Config& config);
 
 //------------------------------------------------------------------------------
 
@@ -62,6 +66,27 @@ main(int argc, char** argv)
   }
   sdsl::simple_sds::load_from(graph, config.graph_name);
 
+  std::unordered_map<gbwt::node_type, std::string> ref_labels = reference_labels(graph, config);
+  auto get_label = [&](handle_t handle) -> std::string
+  {
+    gbwt::node_type node = GBWTGraph::handle_to_node(handle);
+    auto it = ref_labels.find(node);
+    if(it != ref_labels.end())
+    {
+      return ">" + it->second;
+    }
+    it = ref_labels.find(gbwt::Node::reverse(node));
+    if(it != ref_labels.end())
+    {
+      return "<" + it->second;
+    }
+    return ">-";
+  };
+
+  if(config.progress)
+  {
+    std::cerr << "Searching the haplotypes for " << config.pattern << std::endl;
+  }
   std::vector<std::vector<std::string>> buffers(config.threads);
   size_t windows = 0;
   auto flush = [&](size_t thread_id)
@@ -83,6 +108,7 @@ main(int argc, char** argv)
     {
       if(sequence.find(config.pattern) == std::string::npos) { return; }
 
+      // Path in the GFA walk notation.
       std::string line;
       for(handle_t handle : path)
       {
@@ -92,6 +118,7 @@ main(int argc, char** argv)
       }
       line.push_back('\t');
 
+      // Path as a comma-separated list of oriented node labels.
       size_t offset = 0;
       for(handle_t handle : path)
       {
@@ -102,6 +129,7 @@ main(int argc, char** argv)
       }
       line.push_back('\t');
 
+      // Number of haplotypes containing the path.
       // TODO: for_each_haplotype_window should expose the search state.
       gbwt::SearchState state = graph.index.find(GBWTGraph::handle_to_node(path.front()));
       for(size_t i = 1; i < path.size(); i++)
@@ -109,6 +137,23 @@ main(int argc, char** argv)
         state = graph.index.extend(state, GBWTGraph::handle_to_node(path[i]));
       }
       line += std::to_string(state.size());
+
+      // Length of the missing suffix of the last node.
+      line.push_back('\t');
+      line += std::to_string(offset - sequence.length());
+
+      // Reference coordinates for each node, if requested.
+      // If the orientation of the final node is the opposite to the reference,
+      // the coordinates are for the other end of the node. To determine the 
+      // true coordinates, we need to use the previous field.
+      if(!ref_labels.empty())
+      {
+        line.push_back('\t');
+        for(handle_t handle : path)
+        {
+          line += get_label(handle);
+        }
+      }
 
       size_t thread_id = omp_get_thread_num();
       buffers[thread_id].push_back(std::move(line));
@@ -150,9 +195,10 @@ printUsage(int exit_code)
   std::cerr << "Usage: gbz_find [options] graph.gbz pattern > output" << std::endl;
   std::cerr << std::endl;
   std::cerr << "Options:" << std::endl;
-  std::cerr << "  -t, --threads N          use N threads (default and max: " << default_threads() << ")" << std::endl;
-  std::cerr << "  -p, --progress           print progress information" << std::endl;
-  std::cerr << "  -h, --help               print this help message" << std::endl;
+  std::cerr << "  -r, --reference NAME  output coordinates in this reference sample" << std::endl;
+  std::cerr << "  -t, --threads N       use N threads (default and max: " << default_threads() << ")" << std::endl;
+  std::cerr << "  -p, --progress        print progress information" << std::endl;
+  std::cerr << "  -h, --help            print this help message" << std::endl;
   std::cerr << std::endl;
 
   std::exit(exit_code);
@@ -167,6 +213,7 @@ Config::Config(int argc, char** argv) :
   int c = 0, option_index = 0;
   option long_options[] =
   {
+    { "reference", required_argument, 0, 'r' },
     { "threads", required_argument, 0, 't' },
     { "progress", no_argument, 0, 'p' },
     { "help", no_argument, 0, 'h' },
@@ -174,10 +221,13 @@ Config::Config(int argc, char** argv) :
   };
 
   // Process options.
-  while((c = getopt_long(argc, argv, "t:ph", long_options, &option_index)) != -1)
+  while((c = getopt_long(argc, argv, "r:t:ph", long_options, &option_index)) != -1)
   {
     switch(c)
     {
+    case 'r':
+      this->reference_sample = optarg;
+      break;
     case 't':
       try { this->threads = std::stoul(optarg); }
       catch(std::exception& e)
@@ -216,6 +266,49 @@ Config::Config(int argc, char** argv) :
     std::cerr << "Pattern cannot be empty" << std::endl;
     std::exit(EXIT_FAILURE);
   }
+}
+
+//------------------------------------------------------------------------------
+
+std::unordered_map<gbwt::node_type, std::string>
+reference_labels(const GBZ& graph, const Config& config)
+{
+  std::unordered_map<gbwt::node_type, std::string> result;
+  if(config.reference_sample.empty()) { return result; }
+  if(config.progress)
+  {
+    std::cerr << "Determining coordinates in reference sample " << config.reference_sample << std::endl;
+  }
+
+  const gbwt::Metadata& metadata = graph.index.metadata;
+  gbwt::size_type sample = metadata.sample(config.reference_sample);
+  std::vector<gbwt::size_type> paths = metadata.pathsForSample(sample);
+  if(paths.empty())
+  {
+    if(config.progress)
+    {
+      std::cerr << "No paths found for the reference sample" << std::endl;
+    }
+    return result;
+  }
+
+  for(gbwt::size_type path_id : paths)
+  {
+    gbwt::vector_type path = graph.index.extract(gbwt::Path::encode(path_id, false));
+    gbwt::FullPathName path_name = metadata.fullPath(path_id);
+    size_t offset = path_name.offset;
+    std::string pan_sn_name = path_name.sample_name + "#" + std::to_string(path_name.haplotype) + "#" + path_name.contig_name + "#";
+    for(gbwt::node_type node : path)
+    {
+      // TODO: Should we print a warning?
+      if(result.find(node) != result.end()) { continue; }
+      std::string label = pan_sn_name + std::to_string(offset);
+      result[node] = label;
+      offset += graph.graph.get_length(GBWTGraph::node_to_handle(node));
+    }
+  }
+
+  return result;
 }
 
 //------------------------------------------------------------------------------
